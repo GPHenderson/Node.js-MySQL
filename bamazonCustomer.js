d486cf1c4f7dff72d87b4a1fc03d250e0a4488d9
Gregory Henderson <gh_03@yahoo.com>
2019-10-18 21:45:12 -0500

    adding js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,6 +16,81 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connection" + connection.threadId);
+    // console.log("Connection" + connection.threadId);
 
+    displayProducts();
+ 
 });
+ 
+function displayProducts() {
+
+inquirer.prompt({
+  name: "Buy",
+  type: "list",
+  message: "Welcome to Bamazon! Would you like to purchase a product?",
+  choices: ["YES", "EXIT"]
+
+})
+
+.then(function(response){
+
+  if(response.Buy === "YES") {
+    buyProducts();
+  } else if (response.Buy ==="EXIT"){
+    connection.end();
+  }
+
+})
+  var productsView = "SELECT * FROM products";
+
+  connection.query(productsView, function(err, results){
+    if (err) throw err;
+
+      console.table(results)
+    
+  })
+}
+
+function buyProducts() {
+  inquirer.prompt([
+  {
+    name: "ID",
+    type: "input",
+    message: "Select a product by entering the item_id number:",
+    filter: Number
+    
+  },
+  {
+      name: "itemQty",
+      type: "input",
+      message: "Enter Quantity Amount:",
+      filter: Number
+  }
+])
+
+ .then(function(answer) {
+   var unitsNeeded = answer.itemQty;
+   var idNeeded = answer.ID;
+  checkout(unitsNeeded,idNeeded);
+})
+}
+
+    
+ function checkout(ID ,totalNeeded) {
+  connection.query("SELECT * FROM products WHERE item_id = " + ID, function(err, res){
+    if(err){
+      console.log(err)
+    }
+
+    if(totalNeeded<=res[0].stock_quantity){
+      var cost = res[0].price * totalNeeded;
+      connection.query("UPDATE products SET stock_quantity = stock_quantity - " + totalNeeded + " WHERE item_id = " + ID);
+
+      console.log("Your total is " + cost);
+
+      
+    }
+    displayProducts();
+  })
+  
+ }
